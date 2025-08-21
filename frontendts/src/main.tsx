8d96549b3edb09481fb95939966fbe6b0bd3f5d8
Brendan Ashworth <brendan@buntinglabs.com>
2025-08-21 02:13:31 +0000

    wfs/remote url/google sheets support

--- frontendts/src/main.tsx
+++ frontendts/src/main.tsx
@@ -1,3 +1,13 @@
+if (typeof window !== 'undefined' && window.crypto && !window.crypto.randomUUID) {
+  window.crypto.randomUUID = function(): `${string}-${string}-${string}-${string}-${string}` {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    }) as `${string}-${string}-${string}-${string}-${string}`;
+  };
+}
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
